e0a42ba021e5193668ac0bba1bbd832c8d9eba5c
Kyle Fuller <kyle@fuller.li>
2015-03-07 10:54:15 +0000

    Initial project

---
+++ JSONSchema/JSONSchema.swift
@@ -0,0 +1,9 @@
+//
+//  JSONSchema.swift
+//  JSONSchema
+//
+//  Created by Kyle Fuller on 07/03/2015.
+//  Copyright (c) 2015 Cocode. All rights reserved.
+//
+
+import Foundation
---
+++ JSONSchemaTests/JSONSchemaTests.swift
@@ -0,0 +1,36 @@
+//
+//  JSONSchemaTests.swift
+//  JSONSchemaTests
+//
+//  Created by Kyle Fuller on 23/02/2015.
+//  Copyright (c) 2015 Cocode. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class JSONSchemaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
