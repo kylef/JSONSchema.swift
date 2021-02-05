f9801737286001b53fdd3c6b3a3aa9b9e18fcb17
Kyle Fuller <kyle@fuller.li>
2021-02-05 19:18:45 +0000

    refactor: split result into separate file

---
+++ Sources/ValidationResult.swift
@@ -0,0 +1,23 @@
+public typealias ValidationError = String
+public enum ValidationResult {
+  case valid
+  case invalid([ValidationError])
+
+  public var valid: Bool {
+    switch self {
+    case .valid:
+      return true
+    case .invalid:
+      return false
+    }
+  }
+
+  public var errors: [ValidationError]? {
+    switch self {
+    case .valid:
+      return nil
+    case .invalid(let errors):
+      return errors
+    }
+  }
+}
--- Sources/Validators.swift
+++ Sources/Validators.swift
@@ -1,30 +1,6 @@
 import CoreFoundation
 import Foundation
 
-public typealias ValidationError = String
-public enum ValidationResult {
-  case valid
-  case invalid([ValidationError])
-
-  public var valid: Bool {
-    switch self {
-    case .valid:
-      return true
-    case .invalid:
-      return false
-    }
-  }
-
-  public var errors: [ValidationError]? {
-    switch self {
-    case .valid:
-      return nil
-    case .invalid(let errors):
-      return errors
-    }
-  }
-}
-
 /// Creates a Validator which always returns an valid result
 func validValidation(_ value: Any) -> AnySequence<ValidationError> {
   return AnySequence(EmptyCollection())
