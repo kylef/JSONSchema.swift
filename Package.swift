b64e85385ecc778d4c0155494028e20ef9dc17c3
Kevin Lundberg <klundberg@starry.com>
2021-07-02 19:55:45 -0400

    required validation errors show json instance location

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
   name: "JSONSchema",
   platforms: [
-    .macOS(.v10_13),
+    .macOS(.v10_13), .iOS(.v11), .tvOS(.v11)
   ],
   products: [
     .library(name: "JSONSchema", targets: ["JSONSchema"]),
